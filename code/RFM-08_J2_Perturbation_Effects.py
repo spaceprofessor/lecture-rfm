5025d37f2e97114f8da0046eda0c3f23b8b90db5
Bernd Dachwald <dachwald@fh-aachen.de>
2021-11-02 07:19:06 +0100

    Better OOP version for J2 perturbation effect

--- code/RFM-08_J2_Perturbation_Effects.py
+++ code/RFM-08_J2_Perturbation_Effects.py
@@ -1,7 +1,7 @@
 """
 This program was created exclusively for the course "Raumflugmechanik"
 (Spaceflight Dynamics) at FH Aachen University of Applied Sciences
-by Prof. Dr. Bernd Dachwald on 01 November 2021.
+by Prof. Dr. Bernd Dachwald on 03 November 2021.
 """
 
 import numpy as np
@@ -17,58 +17,81 @@ class Earth:
 e = Earth()  # Create Earth object
 
 
-def calculate_prefactor(a, ecc):
-    n = np.sqrt(e.mu / np.power(a, 3))  # Calculate mean motion in [°/s]
-    return -3 * n * e.J2 * np.power(e.R, 2) / (2 * np.power(a, 2) * np.power(1 - np.power(ecc, 2), 2))
+class Orbit:
+    def __init__(self):
+        self.a = 0.0  # Semi-major axis in [km]
+        self.ecc = 0.0  # Eccentricity
+        self.rp = 0.0  # Pericenter radius in [km]
+        self.ra = 0.0  # Apocenter radius in [km]
+        self.n = 0.0  # Mean motion in [rad/s]
+
+    def set_circ_orbit(self, r):
+        self.a = r
+        self.rp = r
+        self.ra = r
+        self.n = np.sqrt(e.mu / np.power(r, 3))
+
+    def set_elliptic_orbit(self, hp, ha):
+        self.rp = e.R + hp
+        self.ra = e.R + ha
+        self.a = (self.rp + self.ra) / 2
+        self.ecc = 1 - self.rp / self.a
+        self.n = np.sqrt(e.mu / np.power(self.a, 3))
 
 
-def rotation_nodal_line(i, h, ecc):  # Calculate dOmega / dt
-    i = i * np.pi / 180  # Convert inclination to [°]
-    ran = calculate_prefactor(e.R + h, ecc) * np.cos(i)  # Calculate dOmega / dt
-    return ran * (180 / np.pi) * 86400  # Convert to [°/d]
+def to_deg_per_day(angle):  # Converts from [rad/s] to [°/d]
+    return angle * (180 / np.pi) * 86400
 
 
-def rotation_apsidal_line(i, a, ecc):  # Calculate domega / dt
-    i = i * np.pi / 180  # Convert inclination to [°]
-    rp = calculate_prefactor(a, ecc) * (5/2 * np.power(np.sin(i), 2) - 2)  # Calculate domega / dt
-    return rp * (180 / np.pi) * 86400  # Convert to [°/d]
+def calculate_prefactor(orb):  # Calculates prefactor in [°/d]
+    pref = -3 * orb.n * e.J2 * np.power(e.R, 2) / (2 * np.power(orb.a, 2) * np.power(1 - np.power(orb.ecc, 2), 2))
+    return to_deg_per_day(pref)
 
 
-def plot_rot_nodal_line(inc, alt, ecc):
+def rotation_nodal_line(i, orb):  # Calculates dOmega / dt in [°/d]
+    return calculate_prefactor(orb) * np.cos(np.deg2rad(i))
+
+
+def rotation_apsidal_line(i, orb):  # Calculates domega / dt in [°/d]
+    return calculate_prefactor(orb) * (5/2 * np.power(np.sin(np.deg2rad(i)), 2) - 2)
+
+
+def plot_rot_nodal_line(inc, orb):
     fig, ax = plt.subplots()
-    for j in range(0, 5):
-        ax.plot(inc, rotation_nodal_line(inc, alt[j], ecc[j]), '-', label='h = {} km'.format(int(alt[j])))
+    for j in range(0, len(orb)):
+        h = int(np.ceil(orb[j].a - e.R))
+        ax.plot(inc, rotation_nodal_line(inc, orb[j]), '-', label='h = {} km'.format(h))
     ax.legend()
 
 
-def plot_rot_apsidal_line(inc, sma, h, ecc):
+def plot_rot_apsidal_line(inc, orb):
     fig, ax = plt.subplots()
-    for j in range(0, 5):
-        ax.plot(inc, rotation_apsidal_line(inc, sma[j], ecc[j]), '-', label='ha = {} km'.format(int(h[j])))
+    for j in range(len(orb)):
+        ha = int(np.ceil(orb[j].ra - e.R))
+        ax.plot(inc, rotation_apsidal_line(inc, orb[j]), '-', label='ha = {} km'.format(ha))
     ax.legend()
 
 
-h_list = [200.0, 500.0, 1000.0, 2500.0, 5000.0]  # List of altitudes in [km]
-hp = 200  # Pericenter altitude for apsidal rotation in [km]
-rp = e.R + hp  # Pericenter radius for apsidal rotation in [km]
-ha_list = [200.0, 500.0, 1000.0, 2500.0, 5000.0]  # List of apocenters for rotation of apsidal line in [km]
-ecc0_list = [0.0] * 5  # List of eccentricities for rotation of nodal line
-ecc_list = [0.0] * 5  # List of eccentricities for rotation od nodal line
-sma_list = [0.0] * 5  # List of semi-major axes for rotation od nodal line
-for j in range(0, 5):
-    sma_list[j] = (rp + (e.R + ha_list[j])) / 2
-    ecc_list[j] = 1 - rp / sma_list[j]
-color_list = ['violet', 'cornflowerblue', 'darkturquoise', 'orange', 'crimson']  # List of plot colors
+h = [200.0, 500.0, 1000.0, 2500.0, 5000.0]  # List of altitudes in [km]
+orbit_nodal_rotation = [Orbit() for j in range(0, len(h))]
+for j in range(0, len(h)):
+    orbit_nodal_rotation[j].set_circ_orbit(e.R + h[j])
 
-inc_range = np.arange(0, 180, 1)  # Range of inclinations in [°]
+hp = 200.0  # Pericenter altitude for apsidal rotation in [km]
+ha = [200.0, 1000.0, 2500.0, 5000.0]  # List of apocenters for apsidal rotation in [km]
+orbit_apsidal_rotation = [Orbit() for j in range(0, len(ha))]
+for j in range(0, len(ha)):
+    orbit_apsidal_rotation[j].set_elliptic_orbit(hp, ha[j])
 
-plot_rot_nodal_line(inc_range, h_list, ecc0_list)
+i = np.arange(0.0, 180.0, 1.0)  # Range of inclinations in [°]
+
+plot_rot_nodal_line(i, orbit_nodal_rotation)
 plt.grid()
 plt.xlabel("Inclination [°]")
 plt.ylabel("Rotation of nodal line [°/d]")
 plt.show()
 
-plot_rot_apsidal_line(inc_range, sma_list, ha_list, ecc_list)
+plot_rot_apsidal_line(i, orbit_apsidal_rotation)
 plt.grid()
 plt.xlabel("Inclination [°]")
 plt.ylabel("Rotation of apsidal line [°/d]")
